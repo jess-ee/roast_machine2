8b9f030f48ae81cb5183f6dc879d5391bff591e8
jess-ee <136972746+jess-ee@users.noreply.github.com>
2023-07-06 20:37:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os 
-from apikey import apikey
 import langchain
 import streamlit as st 
 import time
@@ -19,7 +18,7 @@ from langchain.prompts.chat import (
 from langchain.chains import LLMChain
 event = threading.Event() 
 
-os.environ ['OPENAI_API_KEY'] = apikey
+apikey = os.getenv('OPENAI_API_KEY')
 
 #App framework
 st.title ('🔥Roast Machine 🔥')
